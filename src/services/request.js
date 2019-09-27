e547378d6c3d786770b2f1cf21b9ef68e5debaea
Happytreat <cowfats@gmail.com>
2019-09-27 18:52:33 +0800

    Change url

--- src/services/request.js
+++ src/services/request.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 // TODO: Update when in production
 //const API_URL = 'http://localhost:8010/proxy';
 //const API_URL = 'http://localhost:8000';
-const API_URL = 'https://game-name-api.herokuapp.com';
+//const API_URL = 'https://game-name-api.herokuapp.com';
+const API_URL = 'https://gamenamebackend.com';
 
 // export const isProduction = (process.env.NODE_ENV === 'production');
 //
