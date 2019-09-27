b4660668e9b7aaff53d63904e4822c1d18d8a046
Happytreat <cowfats@gmail.com>
2019-09-27 17:07:23 +0800

    Revert

--- src/consts/routes.js
+++ src/consts/routes.js
@@ -1,7 +1,15 @@
-const baseURL = '/GameNameHosting';
-export const ROUTE_ROOT = `${baseURL}/`;
-export const ROUTE_CREATOR_HOME = `${baseURL}/homepage`;
-export const ROUTE_CREATE_QUESTIONS = `${baseURL}/create/questions`;
-export const ROUTE_CREATE_SUCCESS = `${baseURL}/create/success`;
-export const ROUTE_ADD_PLAYERS = `${baseURL}/game/add/players`;
-export const ROUTE_GAME_ROOM = `${baseURL}/game/play`;
+export const ROUTE_ROOT = "/";
+export const ROUTE_CREATOR_HOME = "/homepage";
+export const ROUTE_CREATE_QUESTIONS = "/create/questions";
+export const ROUTE_CREATE_SUCCESS = "/create/success";
+export const ROUTE_ADD_PLAYERS = "/game/add/players";
+export const ROUTE_GAME_ROOM = "/game/play";
+
+
+// const baseURL = '/GameNameHosting';
+// export const ROUTE_ROOT = `${baseURL}/`;
+// export const ROUTE_CREATOR_HOME = `${baseURL}/homepage`;
+// export const ROUTE_CREATE_QUESTIONS = `${baseURL}/create/questions`;
+// export const ROUTE_CREATE_SUCCESS = `${baseURL}/create/success`;
+// export const ROUTE_ADD_PLAYERS = `${baseURL}/game/add/players`;
+// export const ROUTE_GAME_ROOM = `${baseURL}/game/play`;
