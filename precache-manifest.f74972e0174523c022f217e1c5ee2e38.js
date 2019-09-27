434cd6a62d32e18e9eb1fbfaf1c3d171dc5605ab
Happytreat <cowfats@gmail.com>
2019-09-27 16:19:58 +0800

    Updates

---
+++ precache-manifest.f74972e0174523c022f217e1c5ee2e38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62a48282357847b592aa5d2e992ced1",
+    "url": "/GameNameHosting/index.html"
+  },
+  {
+    "revision": "522edd0fcad344703f55",
+    "url": "/GameNameHosting/static/css/main.bd2e36d9.chunk.css"
+  },
+  {
+    "revision": "a720ab02f7cb7d6bacff",
+    "url": "/GameNameHosting/static/js/2.4fe34802.chunk.js"
+  },
+  {
+    "revision": "522edd0fcad344703f55",
+    "url": "/GameNameHosting/static/js/main.6b72eaa4.chunk.js"
+  },
+  {
+    "revision": "fe9a45dc4c365c708ed1",
+    "url": "/GameNameHosting/static/js/runtime~main.90e84ef3.js"
+  },
+  {
+    "revision": "33c655af3b8465a4259b9fd790099029",
+    "url": "/GameNameHosting/static/media/404.33c655af.svg"
+  },
+  {
+    "revision": "bed2ef9a684b4ba5d9c195cb9ab39233",
+    "url": "/GameNameHosting/static/media/Background.bed2ef9a.png"
+  }
+]);
